1e7f0ab0928739e55efcdaf176afd6e984b16f63
xiaoxiao <xingchen@maihaoche.com>
2018-05-20 10:31:08 +0800

    构建订单服务提供项目

---
+++ wechatOrder-Order-Api/src/main/java/DO/BaseInfo.java
@@ -0,0 +1,45 @@
+package DO;
+
+import java.io.Serializable;
+
+public class BaseInfo implements Serializable {
+
+
+    private static final long serialVersionUID = 4327471841751252435L;
+    private  String buyerAddress;
+    private String buyerOpenId;
+   private String sellId;
+
+    public BaseInfo() {
+    }
+
+    public BaseInfo(String buyerAddress, String buyerOpenId, String sellId) {
+        this.buyerAddress = buyerAddress;
+        this.buyerOpenId = buyerOpenId;
+        this.sellId = sellId;
+    }
+
+    public String getBuyerAddress() {
+        return buyerAddress;
+    }
+
+    public void setBuyerAddress(String buyerAddress) {
+        this.buyerAddress = buyerAddress;
+    }
+
+    public String getBuyerOpenId() {
+        return buyerOpenId;
+    }
+
+    public void setBuyerOpenId(String buyerOpenId) {
+        this.buyerOpenId = buyerOpenId;
+    }
+
+    public String getSellId() {
+        return sellId;
+    }
+
+    public void setSellId(String sellId) {
+        this.sellId = sellId;
+    }
+}
